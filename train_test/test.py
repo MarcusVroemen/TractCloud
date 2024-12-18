6bfb20af5277562049d6b06a9ca9a62443e21ac0
Marcus Vroemen <m.j.vroemen@student.tue.nl>
2024-12-18 13:55:16 +0000

    multitask for testing

--- train_test/test.py
+++ train_test/test.py
@@ -34,10 +34,18 @@ def load_batch_data():
         label_names =  list(ordered_tract_cluster_mapping_dict.keys()) 
     else:
         label_names = test_dataset.label_names
-    num_classes = [len(np.unique(label_names[0])), len(np.unique(label_names[1]))]
-    logger.info('The number of classes is:{}'.format(num_classes[0]))
-    logger.info('The number of classes is:{}'.format(num_classes[1]))
-
+        
+    num_classes=[]
+    if len(args.atlas)==1:
+        num_classes=len(np.unique(label_names))
+        logger.info('The number of classes is for {} is: {}'.format(args.atlas[0], num_classes))
+    elif len(args.atlas)==2: 
+        num_classes = [len(np.unique(label_names[0])), len(np.unique(label_names[1]))]
+        logger.info('The number of classes is for {} is: {}'.format(args.atlas[0], num_classes[0]))
+        logger.info('The number of classes is for {} is: {}'.format(args.atlas[1], num_classes[1]))
+    else:
+        raise ValueError("Currently, only supports one or two atlases.")
+    
     # test random feat
     test_global_feat = test_dataset.global_feat
     
@@ -52,15 +60,32 @@ def test_DL_net(net):
     total_test_loss = 0
     test_labels_lst_0, test_predicted_lst_0 = [], []
     test_labels_lst_1, test_predicted_lst_1 = [], []
+    inference_times = []  # List to store inference times for each sample
+
     # test
     with torch.no_grad():
         for j, data in enumerate(test_loader, start=0):
+            start_time = time.time()  # Start time for the batch
             total_loss, labels_lst_0, predicted_lst_0, labels_lst_1, predicted_lst_1 = \
                 train_val_test_forward(j, data, net, 'test', total_test_loss, test_labels_lst_0, test_predicted_lst_0,
                                        test_labels_lst_1, test_predicted_lst_1, args, device, num_classes, epoch=1, eval_global_feat=test_global_feat)
-    
+            end_time = time.time()  # End time for the batch
+
+            batch_inference_time = end_time - start_time
+            num_samples = data[0].size(0)  # Assuming data[0] contains the inputs
+            per_sample_time = batch_inference_time / num_samples
+
+            inference_times.extend([per_sample_time] * num_samples)  # Add per-sample times to the list
+
+    # Calculate average and standard deviation of inference times per sample
+    avg_time = np.mean(inference_times)
+    std_time = np.std(inference_times)
+
+    logger.info(f"Average inference time per sample: {avg_time:.6f} seconds")
+    logger.info(f"Standard deviation of inference time per sample: {std_time:.6f} seconds")
+
     return labels_lst_0, predicted_lst_0, labels_lst_1, predicted_lst_1
-    
+
     
 def test_paths():
     # paths
@@ -80,6 +105,11 @@ if __name__ == '__main__':
     time_start = time.time()
     # GPU check
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    if not torch.cuda.is_available():
+        print("Error: CUDA device not available. Exiting program.")
+        sys.exit(1)  # Exit with error code 1
+
     # Variable Space
     parser = create_parser()
     args = parser.parse_args()
@@ -91,15 +121,10 @@ if __name__ == '__main__':
     args.manualSeed = 0 
     print("Random Seed: ", args.manualSeed)
     fix_seed(args.manualSeed)
-    #
-    # args.input_path="/media/volume/HCP_diffusion_MV/TrainData_MRtrix_100_symmetric" #TODO why is this necessary
-    # print(args.input_path, args.connectome)
     # adaptively change the args
     args = adaptive_args(args)
     # paths
     test_paths()
-    # Tract cluster mapping
-    ordered_tract_cluster_mapping_dict = obtain_TractClusterMapping()  # {'tract name': ['cluster_xxx','cluster_xxx', ... 'cluster_xxx']} 
     # Record the training process and values
     logger = create_logger(args.out_log_path)
     logger.info('=' * 55)
@@ -107,26 +132,21 @@ if __name__ == '__main__':
     logger.info('=' * 55)
     # load data
     test_loader, label_names, num_classes, test_data_size, eval_state, test_global_feat, samples_per_class = load_batch_data()
-    
-    # Test on best org and tract weights separately
-    if args.connectome:
-        weight_prefix_lst = ['org']
-    else:
-        weight_prefix_lst = ['org', 'tract']
-        
     time_DL_start = time.time()
-    for weight_prefix in weight_prefix_lst:  # load best weight one by one
-        # model setting
-        args.weight_path = os.path.join(args.out_path, 'best_{}_{}_model.pth'.format(weight_prefix, args.best_metric))
-        logger.info("Load best {} {} model".format(weight_prefix, args.best_metric))
-        DL_model = load_model(args, num_classes, device, test=True)
-        # test net
-        labels_lst_0, predicted_lst_0, labels_lst_1, predicted_lst_1 = test_DL_net(DL_model)
+    # model setting
+    args.weight_path = os.path.join(args.out_path, 'best_{}_model.pth'.format(args.best_metric))
+    logger.info("Load best {} model".format(args.best_metric))
+    DL_model = load_model(args, num_classes, device, test=True)
+    # test net
+    labels_lst_0, predicted_lst_0, labels_lst_1, predicted_lst_1 = test_DL_net(DL_model)
     time_DL_end = time.time()
     # log results
     results_logging(args, logger, eval_state, label_names[0], labels_lst_0, predicted_lst_0, "aparc+aseg")
-    results_logging(args, logger, eval_state, label_names[1], labels_lst_1, predicted_lst_1, "aparc.a2009s+aseg")
+    if len(args.atlas)==2:
+        results_logging(args, logger, eval_state, label_names[1], labels_lst_1, predicted_lst_1, "aparc.a2009s+aseg")
 
+    args.root = "/media/volume/MV_HCP/TrainData_MRtrix_1000_MNI_100K/"
+    print(args.root)
     # total processing time
     time_end = time.time() 
     total_time_DL = round(time_DL_end-time_DL_start, 2)
--- train_test/test_realdata.py
+++ train_test/test_realdata.py
@@ -21,15 +21,15 @@ from utils.metrics_connectome import *
 
 def test_realdata_DL_net(net):
     """test the network"""
-    test_labels_lst, test_predicted_lst = [], []
+    test_predicted_lst_0, test_predicted_lst_1 = [], []
     # test
     with torch.no_grad():
         for j, data in enumerate(test_loader, start=0):
-            _, _, test_predicted_lst = \
-                train_val_test_forward(j, data, net, 'test_realdata', -1, [], test_predicted_lst,
+            _, _, test_predicted_lst_0, _, test_predicted_lst_1 = \
+                train_val_test_forward(j, data, net, 'test_realdata', -1, [], test_predicted_lst_0, [], test_predicted_lst_1,
                                        args, device, args.num_classes, epoch=1, eval_global_feat=test_realdata_global_feat)
 
-    return test_labels_lst, test_predicted_lst
+    return test_predicted_lst_0, test_predicted_lst_1
 
 
 start_time = time.time()
@@ -63,10 +63,8 @@ parser.add_argument('--num_classes', type=int, default=1600, help='number of cla
 parser.add_argument('--cal_equiv_dist', default=False, action='store_true', help='Calculate equivalent distance for pairwise distance matrix for finding neighbors')
 parser.add_argument('--k_ds_rate', type=float, default=0.1, help='downsample the tractography when calculating pairwise distance matrix')
 
-parser.add_argument('--connectome', default=False, action='store_true', help='Testing on data returns connectomes and metrics')
 parser.add_argument('--atlas', type=str, default='aparc+aseg', help='Atlas used')
-parser.add_argument('--class_weighting', type=float, default=0, help='Class weights, 0 is no weights, scales quadratically')
-parser.add_argument('--encoding', type=str, default='default', help='Method of encoding 2D connectome labels')
+parser.add_argument('--save_parcellated', default=False, action='store_true', help='save parcellated streamlines per class')
 parser.add_argument('--fibersampling', type=float, default=0, help='Distance in fiber sampling, higher means points higher distribution near ends')
     
 
@@ -76,10 +74,7 @@ args_path = args.weight_path_base + '/cli_args.txt'
 # input from train.py keyboard, in cli.txt
 args = load_args_in_testing_only(args_path, args)
 # paths
-if not args.connectome:
-    args.weight_path = os.path.join(args.weight_path_base, 'best_tract_f1_model.pth')
-else:
-    args.weight_path = os.path.join(args.weight_path_base, 'best_org_f1_model.pth')
+args.weight_path = os.path.join(args.weight_path_base, 'best_f1_model.pth')
     
 makepath(args.out_path)
 # create logger
@@ -89,94 +84,96 @@ logger = create_logger(log_path)
 logger.info('=' * 55)
 logger.info(args)
 logger.info('=' * 55)
-args.atlas='aparc+aseg'
+
 # label names
-if not args.connectome:
-    ordered_tract_cluster_mapping_dict = obtain_TractClusterMapping()
-    tract_label_names_str = list(ordered_tract_cluster_mapping_dict.keys())
-else:
-    label_names_tuple = list(generate_label_dict(85, 'symmetric'))
-    label_names_str=[]
+num_labels = {"aparc+aseg": 85, "aparc.a2009s+aseg": 165}
+label_names = {}
+
+for atlas in args.atlas:
+    label_names_tuple = list(generate_label_dict(num_labels[atlas], 'symmetric'))
+    label_names_str = []
     for label in label_names_tuple:
-        label_names_str.append(str(label[0]) + '_' + str(label[1]))
+        label_names_str.append(f"{label[0]}_{label[1]}")
+    label_names[atlas] = label_names_str
 
 # read tractography into feature
 time_start_read = time.time()
 pd_tractography = wma.io.read_polydata(args.tractography_path)
-logger.info('Finish reading tractography from: {}, took {} s'.format(args.tractography_path, time.time()-time_start_read))
+logger.info('Finish reading tractography from: {}, took {} s'.format(args.tractography_path, round(time.time()-time_start_read, 3)))
 tractography_file_n = os.path.basename(os.path.normpath(args.tractography_path))
 
 # load non-registered feature 
 time_start_load = time.time()
-logger.info('Extracting feature from unregistered tractography'.format(args.tractography_path))
+logger.info('Extracting features from tractography'.format(args.tractography_path))
 feat_RAS, _ = tract_feat.feat_RAS(pd_tractography, number_of_points=args.num_points, decay_factor=args.fibersampling)
-logger.info('The number of fibers in test tractography is {}'.format(feat_RAS.shape[0]))
-logger.info('Extracting RAS features is done, took {} s'.format(time.time()-time_start_load))
-
-# re-center tractography
-# time_start_recenter = time.time()
-# save_recentered_data = False
-# centered_feat_RAS = center_tractography(args.input_path,feat_RAS,args.out_path,
-#                                         logger,tractography_file_n,save_recentered_data)
-# logger.info('re-centering tractography is done, took {} s'.format(time.time()-time_start_recenter))
-centered_feat_RAS=feat_RAS
+logger.info('The number of fibers in tractography is {}'.format(feat_RAS.shape[0]))
+logger.info('Extracting RAS features is done, took {} s'.format(round(time.time()-time_start_load, 3)))
 
 # Real data processing
 time_start_processing = time.time()
-test_realdata = RealData_PatchData(centered_feat_RAS, k=args.k, k_global=args.k_global, cal_equiv_dist=args.cal_equiv_dist, 
+test_realdata = RealData_PatchData(feat_RAS, k=args.k, k_global=args.k_global, cal_equiv_dist=args.cal_equiv_dist, 
                                     use_endpoints_dist=False, rough_num_fiber_each_iter=args.num_fiber_per_brain, k_ds_rate=args.k_ds_rate) 
 test_loader = torch.utils.data.DataLoader(test_realdata, batch_size=args.test_realdata_batch_size, shuffle=False)
 test_realdata_global_feat = test_realdata.global_feat
 test_realdata_size = len(test_realdata)
-logger.info('calculating knn+random features is done, took {} s'.format(time.time()-time_start_processing))
+logger.info('calculating knn+random features is done, took {} s'.format(round(time.time()-time_start_processing, 3)))
 
 # test network
 time_start_model = time.time()
-DL_model = load_model(args, num_classes=args.num_classes, device=device, test=True)  
-_, pred_labels = test_realdata_DL_net(DL_model)
-logger.info('Inference time is {}s'.format(time.time()-time_start_model))
-
-
-if not args.connectome:
-    time_start_write = time.time()
-    tract_pred_labels = cluster2tract_label(pred_labels, ordered_tract_cluster_mapping_dict)
-    logger.info('Deep learning prediction is done.')
-    # save parcellated vtp results
-    tractography_parcellation(args, pd_tractography, tract_pred_labels, tract_label_names_str)  # pd tractography here is in the subject space.
-    logger.info('Time to write vtks is {}s'.format(time.time()-time_start_write))
-else:
-    # Save predictions
-    # time_start_write = time.time()
-    # tractography_parcellation(args, pd_tractography, pred_labels, label_names_str)  # pd tractography here is in the subject space.
-    # logger.info('Time to write vtks is {}s'.format(time.time()-time_start_write))
+if len(args.atlas)==1:
+    DL_model = load_model(args, num_classes=len(label_names[args.atlas[0]]), device=device, test=True)  
+    pred_labels_0 = test_realdata_DL_net(DL_model)
     
-    time_start_metrics = time.time()
-    connectome_path = os.path.join(args.out_path, 'connectome')
-    makepath(connectome_path)
-    pred_labels_decoded = convert_labels_list(pred_labels, encoding_type=args.encoding, 
-                                                mode='decode', num_labels=85)
+elif len(args.atlas)==2: 
+    DL_model = load_model(args, num_classes=[len(label_names[args.atlas[0]]), len(label_names[args.atlas[1]])], 
+                          device=device, test=True)  
+    pred_labels_0, pred_labels_1 = test_realdata_DL_net(DL_model)
+else:
+    raise ValueError("Currently, only supports one or two atlases.")
+logger.info('Inference time is {}s'.format(round(time.time()-time_start_model, 3)))
 
-    # Write prediction files
-    with open(os.path.join(connectome_path, f'predictions_{args.atlas}.txt'), 'w') as file:
+if args.save_parcellated:
+    # Save predictions per class
+    time_start_write = time.time()
+    tractography_parcellation(args, pd_tractography, pred_labels, label_names)  # pd tractography here is in the subject space.
+    logger.info('Time to write vtks is {}s'.format(round(time.time()-time_start_write, 3)))
+    
+# Save predictions
+for i, atlas in enumerate(args.atlas):
+    # Select the correct predictions based on the atlas index
+    pred_labels = pred_labels_0 if i == 0 else pred_labels_1
+    num_labels_atlas = num_labels[atlas]  # Use the number of labels specific to each atlas
+
+    # Decode the predicted labels
+    pred_labels_decoded = convert_labels_list(pred_labels, encoding_type='symmetric', 
+                                              mode='decode', num_labels=num_labels_atlas)
+
+    # Write decoded predictions to a file
+    with open(os.path.join(args.out_path, f'predictions_{atlas}.txt'), 'w') as file:
         for prediction in pred_labels_decoded:
-            file.write(f'{prediction}\n')
-    with open(os.path.join(connectome_path, f'predictions_{args.atlas}_{args.encoding}.txt'), 'w') as file:
+            file.write(f'{prediction[0]} {prediction[1]}\n')
+
+    # Write original predictions (without decoding) to a file for reference
+    with open(os.path.join(args.out_path, f'predictions_{atlas}_symmetric.txt'), 'w') as file:
         for prediction in pred_labels:
             file.write(f'{prediction}\n')
-    
-    # Read true files
-    encode_labels_txt(os.path.join(os.path.dirname(args.tractography_path), f"labels_{args.atlas}.txt"), 
-                      os.path.join(os.path.dirname(args.tractography_path), f"labels_{args.atlas}_{args.encoding}.txt"), 'symmetric', num_labels=85)
-    with open(os.path.join(os.path.dirname(args.tractography_path), f"labels_{args.atlas}_{args.encoding}.txt"), 'r') as file:
-        true_labels = [int(line) for line in file]
-    
-    # Plot connectomes, and compute metrics
-    CM = ConnectomeMetrics(true_labels=true_labels, pred_labels=pred_labels, encoding='symmetric', out_path=connectome_path)
-    logger.info(CM.format_metrics())
-    logger.info('Time to compute metrics and plot connectomes {}s'.format(time.time()-time_start_metrics))
 
+    # Read true labels file for the current atlas
+    encode_labels_txt(
+        os.path.join(os.path.dirname(args.tractography_path), f"labels_10M_{atlas}.txt"),
+        os.path.join(os.path.dirname(args.tractography_path), f"labels_10M_{atlas}_symmetric.txt"),
+        'symmetric', num_labels=num_labels_atlas
+    )
+
+    # with open(os.path.join(os.path.dirname(args.tractography_path), f"labels_10M_{atlas}_symmetric.txt"), 'r') as file:
+    #     true_labels = [int(line) for line in file]
+
+    # # Compute connectome metrics for each atlas
+    # CM = ConnectomeMetrics(true_labels=true_labels, pred_labels=pred_labels, atlas=atlas, out_path=args.out_path, graph=True)
+    # logger.info(CM.format_metrics())
+    
 # time
 end_time = time.time()
 tot_time = round(end_time - start_time, 3)
-logger.info('All done!!! Total time is {}s'.format(tot_time))
+logger.info('All done!!! Total time is {}s'.format(round(tot_time, 3)))
     
